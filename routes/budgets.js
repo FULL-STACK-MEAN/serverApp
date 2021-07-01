a7cb28cf5f6a0c620042676cfffd0c21578583a0
getafemean <getafemean@gmail.com>
2021-07-01 20:47:38 +0200

    email template

--- routes/budgets.js
+++ routes/budgets.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+const nodemailer = require('nodemailer');
+
 const { tokenVerification } = require('../middleware/tokenverification');
 const { ErrorHandler } = require('../helpers/errors');
 const { createBudget, getBudgets, getBudget, updateBudget } = require('../services/budgets');
@@ -29,6 +32,79 @@ app.get('/createpdf/:_id', tokenVerification, async (req, res, next) => {
             message: 'El PDF fue generado correctamente'
         })
     } catch (err) {
+        return next(err);
+    }
+})
+
+app.get('/sendemail/:_id', tokenVerification, async (req, res, next) => {
+    try {
+        if(req.params._id === undefined) {
+               throw new ErrorHandler(404, '_id param is mandatory')
+        }
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'getafemean@gmail.com',
+                pass: 'NoHay2sin3*'
+            }
+        })
+
+        const budget = await getBudget(req.params._id);
+        // En el caso de Gmail, autorizar desde https://myaccount.google.com/lesssecureapps
+        const budgetEmail =  {
+            from: 'getafemean@gmail.com',
+            to: budget.customer.contact.email,
+            subject: 'Su presupuesto solicitado',
+            html: `
+            <body style="margin: 0; padding: 20px; background: grey">
+                <table style="display: block; width: 600px; max-width: calc(100% - 40px); margin: 20px auto; padding: 20px; background: white">
+                    <tbody style="display: block; width: 100%;">
+                        <tr style="display: block; width: 100%;">
+                            <td style="display: block; width: 100%; text-align: center; ">
+                                <img src="http://localhost:3000/pics/logo.svg" style="height: 80px">
+                            </td>
+                        </tr>
+                        <tr style="display: block; width: 100%;">
+                            <td style="display: block; width: 100%;">
+                                <p style="font-family: Arial, Helvetica, sans-serif;">Estimado cliente,</p>
+                                <p style="font-family: Arial, Helvetica, sans-serif;">Adjunto a este correo le enviamos el presupuesto solicitado recientemente. Le rogamos se ponga en contacto con nosotros para cualquier duda o consulta adicional.</p>
+                            </td>
+                        </tr>
+                        <tr style="display: block; width: 100%;">
+                            <td style="display: block; width: 100%;">
+                                <a href="http://localhost:3000/pdfs/${budget.code}.pdf" target="_blank" style="background-color: brown; color: white; font-family:Arial, Helvetica, sans-serif; border: none; border-radius: 5px; padding: 5px 10px;">Descargar presupuesto</a>
+                            </td>
+                        </tr>
+                        <tr style="display: block; width: 100%;">
+                            <td style="display: block; width: 100%;">
+                                <p style="font-family: Arial, Helvetica, sans-serif;">Atentamente</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </body>
+            `,
+            attachments: [
+                {
+                    path: 'budgetsPDF/021-2021.pdf',
+                    contentType: 'application/pdf'
+                }
+            ]
+        }
+
+        transporter.sendMail(budgetEmail, (err, info) => {
+            if(err) {
+                console.log(err)
+            } else {
+                console.log(info)
+            }
+        })
+
+        res.status(200).json({
+            message: 'El presupuesto fue enviado correctamente'
+        })
+
+    } catch(err) {
         return next(err);
     }
 })
